69e02a9e4815b93a18206eaadf04fb91ed563084
naacho17 <isere@correo.um.edu.uy>
2025-05-26 15:35:07 -0300

    pasar clases hijas al mismo archivo

--- Clases/Empresa.py
+++
@@ -1,8 +0,0 @@
-from Clases.Cliente import Cliente
-
-class Empresa(Cliente):
-    def __init__(self, id, rut, nombre, web, telefono, email):
-        super().__init__(id, telefono, email)
-        self.rut = rut
-        self.nombre = nombre
-        self.web = web
